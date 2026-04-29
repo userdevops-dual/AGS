d3aebe2a9154548acbbc884999de07601b52f7f9
Abdul Raheem <userme782.dev@gmail.com>
2026-04-29 22:37:14 +0500

    Configure GitHub Pages deployment with GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/ags',
+  assetPrefix: '/ags',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
